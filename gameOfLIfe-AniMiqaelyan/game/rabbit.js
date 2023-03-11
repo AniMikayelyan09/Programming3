21b379bf3c31ef68954411a828c36a203b1cee42
AniMikayelyan09 <ani.mikayelyan2@tumo.org>
2023-03-11 16:13:26 +0400

    first commit

---
+++ gameOfLIfe-AniMiqaelyan/game/rabbit.js
@@ -0,0 +1,120 @@
+class Rabbit{
+    constructor(x,y){
+              this.x = x
+              this.y = y
+              this.energy = 20
+              this.directions = []
+    }
+
+
+    getNewCoordinates() {
+      this.directions = [
+          [this.x - 1, this.y - 1],
+          [this.x, this.y - 1],
+          [this.x + 1, this.y - 1],
+          [this.x - 1, this.y],
+          [this.x + 1, this.y],
+          [this.x - 1, this.y + 1],
+          [this.x, this.y + 1],
+          [this.x + 1, this.y + 1]
+      ];
+  }
+  chooseCell(char) {
+      this.getNewCoordinates();
+      let found = [];
+
+      for (let i in this.directions) {
+          let x = this.directions[i][0];
+          let y = this.directions[i][1];
+
+          if (y < matrix.length && y >= 0 && x < matrix[0].length && x >= 0) {
+              if (matrix[y][x] == char) {
+                  found.push(this.directions[i]);
+              }
+          }
+
+      
+        }    
+
+          
+      
+
+      return found;
+  }
+
+  mul() {
+    let emptyCell = this.chooseCell(0);
+    let newCell = random(emptyCell)
+
+    if (newCell && this.energy > 5) {
+        let newX = newCell[0];
+        let newY = newCell[1];
+
+        let rab = new Rabbit(newX, newY);
+        matrix[newY][newX] = 5;
+       rabbitArr.push(rab);
+
+        this.energy = 20;
+    }
+}
+
+eat() {
+    let emptyCell = this.chooseCell(1);
+    let newCell = random(emptyCell)
+
+    if (newCell) {
+        this.energy += 5;
+        let newX = newCell[0];
+        let newY = newCell[1];
+
+        for (let i = 0; i < grassArr.length; i++) {
+            if (grassArr[i].x == newX && grassArr[i].y == newY) {
+                grassArr.splice(i, 1)
+                break;
+            }
+        }
+
+        matrix[newY][newX] = 5;
+        matrix[this.y][this.x] = 0;
+
+        this.x = newX;
+        this.y = newY;
+
+        if (this.energy > 30) {
+            this.mul()
+        }
+     } else {
+        this.move()
+    }
+}
+move() {
+    let emptyCell = this.chooseCell(0);
+    let newCell = random(emptyCell)
+
+    if (newCell) {
+        let newX = newCell[0];
+        let newY = newCell[1];
+
+        matrix[newY][newX] = 5;
+        matrix[this.y][this.x] = 0;
+
+       
+        this.x = newX;
+        this.y = newY;
+
+        this.energy--
+
+        if (this.energy < 0) {
+            this.die()
+        }
+    } 
+}
+die() {
+    for (let i = 0; i < rabbitArr.length; i++) {
+        if (rabbitArr[i].x == this.x && rabbitArr[i].y == this.y) {
+           rabbitArr.splice(i, 1)
+        }
+    }
+    matrix[this.y][this.x] = 0
+}
+}
