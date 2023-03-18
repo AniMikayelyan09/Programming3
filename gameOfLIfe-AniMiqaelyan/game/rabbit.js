bd8215ebd5f8e049a0bd56865cd7b7e4211b2af8
AniMikayelyan09 <ani.mikayelyan2@tumo.org>
2023-03-18 16:22:15 +0400

    moduls

--- gameOfLIfe-AniMiqaelyan/game/rabbit.js
+++ gameOfLIfe-AniMiqaelyan/game/rabbit.js
@@ -1,4 +1,5 @@
-class Rabbit extends LivingCreature{
+let LivingCreature = require("./LivingCreature ")
+module.exports = class Rabbit extends LivingCreature{
     constructor(x,y){
               super(x,y)
               this.energy = 20
